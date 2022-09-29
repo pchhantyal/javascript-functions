53b2c3b4d87f22a0f089886c01d9d94a16193a9f
pchhantyal <114522335+pchhantyal@users.noreply.github.com>
2022-09-28 21:59:02 -0500

    'seed'

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -49,15 +49,41 @@ const getNeighborsOf = ([x, y]) => {
   [x-1, y-1], [x, y-1], [x+1, y-1]
 };
 
-const getLivingNeighbors = (cell, state) => {};
+const getLivingNeighbors = (cell, state) => {
+  return getNeighborsOf(cell).filter((n) => contains.bind(state)(n));
+};
 
-const willBeAlive = (cell, state) => {};
+const willBeAlive = (cell, state) => {
+  const livingNeighbors = getLivingNeighbors(cell, state);
 
-const calculateNext = (state) => {};
+  return (
+    livingNeighbors.length === 3 ||
+    (contains.call(state,cell) && livingNeighbors.length == 2)
+  );
+};
 
-const iterate = (state, iterations) => {};
+const calculateNext = (state) => {
+  const { bottomLeft, topRight } = corners(state);
+  let result = [];
+  for (let y = topRight[1] + 1; y >= bottomLeft[1] - 1; y--) {
+    for (let x = bottomLeft[0] - 1; x <= topRight[0] + 1; x++) {
+      result = result.concat(willBeAlive([x,y], state) ? [[x, y]] : []);
+    }
+  }
+};
 
-const main = (pattern, iterations) => {};
+const iterate = (state, iterations) => {
+const states = [state];
+for (let i = 0; i < iterations; i++) {
+  states.push(calculateNext(states[states.length - 1]));
+ }
+ return states;
+};
+
+const main = (pattern, iterations) => {
+  const results = iterate(startPatterns[pattern], iterations);
+  results.forEach(r => console.log(printCells(r)));
+};
 
 const startPatterns = {
     rpentomino: [
